7c9e5bc754f77060477ee1710a9068d5741393c5
Aaron Beckman <aaronpbeckman@gmail.com>
2019-08-25 23:26:40 -0500

    Update left-hand-mouse.ino

--- left-hand-mouse/left-hand-mouse.ino
+++ left-hand-mouse/left-hand-mouse.ino
@@ -1,12 +1,24 @@
 #include <HID-Project.h>
-#include <HID-Settings.h>
 
-#define 
+const int HALL_EFFECT = 2;
+int ticks = 0;
 
 void setup() {
   Serial.begin(9600);
+  Consumer.begin();
+  
+  pinMode(HALL_EFFECT, INPUT);
+  attachInterrupt(digitalPinToInterrupt(HALL_EFFECT), tick, FALLING);
 }
 
 void loop() {
-  
+  Serial.println("Waiting for input | " + String(ticks) + " | " + digitalRead(HALL_EFFECT));
+  delay(100);
 }
+
+void tick() {
+  Serial.println("Tick, tock, I'm a clock | " + String(++ticks));
+  if(ticks % 2 == 0) Consumer.write(MEDIA_VOL_UP);
+  else Consumer.write(MEDIA_VOL_DOWN);
+}
+
